811a6b6e2856f8fc7eae0327a2a610b53043610f
Balaji Wanave <balajiwanave1990@gmail.com>
2021-05-08 17:09:53 +0530

    #1 Micronaut example

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,10 +1,16 @@
 package com.example;
 
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+        LOGGER.info("\"Micronaut-Example\" application started successfully !!");
+        LOGGER.info("Machine configuration: [Available processors: {}]", Runtime.getRuntime().availableProcessors());
     }
 }
